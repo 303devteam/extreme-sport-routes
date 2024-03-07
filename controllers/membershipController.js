b6875b7c16bc60990bd3212bddd57ea3d03d9c5c
naimPJ <private.naim@gmail.com>
2024-03-08 00:22:40 +0100

    zavrsio bekend, ja mislim

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -46,19 +46,48 @@ const MembershipController = {
             
             const memberId = req.params.id; 
             const membership = await Membership.findOne({ where: { memberId } });
-            
+
             if (!membership) {
                 return res.status(404).json({ error: 'Membership not found' });
             }
             const currentDate = new Date();
             const endDate = new Date(membership.endDate);
             if (endDate < currentDate) {
-                return res.json({ isValid: false, message: 'Membership is expired' });
+                return res.json({ isValid: false, message: 'Membership is expired', currentDate, endDate: membership.endDate});
+                
             }
             res.json({ isValid: true, message: 'Membership is valid', currentDate, endDate: membership.endDate});
             
         } catch (error) {
             console.error('Error checking membership validity:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+
+    extendMembership: async (req, res) => {
+        try {
+            const memberId = req.params.id;
+            const membership = await Membership.findOne({ where: { memberId } });
+            
+            if (!membership) {
+                return res.status(404).json({ error: 'Membership not found' });
+            }
+
+            const currentDate = new Date();
+            const endDate = new Date(membership.endDate);
+
+            if (endDate < currentDate) {
+               
+                const newEndDate = new Date(endDate);
+                newEndDate.setDate(newEndDate.getDate() + membership.numberOfSessions);
+                await membership.update({ endDate: newEndDate });
+                return res.json({ message: 'Membership extended', newEndDate });
+            }
+          
+
+         
+        } catch (error) {
+            console.error('Error extending membership:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     }
--- member/membershipRoutes.js
+++ member/membershipRoutes.js
@@ -9,5 +9,6 @@ router.post("/addmemship", MembershipController.addMembership);
 router.put("/updatememship/:id", MembershipController.updateMembership);
 router.delete("/delmemship/:id", MembershipController.deleteMembership);
 router.get("/checkmemship/:id", MembershipController.checkMembershipValidity);
+router.put("/extendmemship/:id", MembershipController.extendMembership);
 
 export default router;
